eef2431e28586511ae9008d709dbf4588ea32108
ygvbhy <ygvbhy@naver.com>
2025-05-04 23:57:12 +0900

    API Add

--- api/index.js
+++ api/index.js
@@ -46,6 +46,44 @@ app.get("/api/read-hot-post", async (req, res) => {
   }
 });
 
+app.get("/api/save-new-post", async (req, res) => {
+  console.log("📩 /save-new-post route hit");
+  try {
+    const { data } = await axios.get("http://13.125.208.179:5011/channels");
+
+    const channelsData = data.map((item) => item._id);
+    let newPosts = [];
+
+    for (const channel of channelsData) {
+      const { data } = await axios.get(
+        `http://13.125.208.179:5011/posts/channel/${channel}?limit=100&sort=createdAt:desc`
+      );
+      newPosts = [...newPosts, ...data];
+    }
+
+    newPosts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    const newPostsData = newPosts.slice(0, 8);
+
+    const filePath = path.join("/tmp", "new-post.json");
+    await fs.writeFile(filePath, JSON.stringify(newPostsData, null, 2));
+
+    res.status(200).send("New post saved!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.get("/api/read-new-post", async (req, res) => {
+  try {
+    const filePath = path.join("/tmp", "new-post.json");
+    const content = await fs.readFile(filePath, "utf-8");
+    res.status(200).json(JSON.parse(content));
+  } catch (error) {
+    res.status(404).send("❌ new-post.json not found");
+  }
+});
+
 // Catch-all
 app.use((req, res) => {
   res.status(404).send("❌ Not found");
